599f273cb6b643523554cdd53d6a890388fa332d
moqilin <1637774302@qq.com>
2022-08-02 15:28:44 +0800

    asdasdas

---
+++ src/main/java/com/example/gitcode/GitcodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitcodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitcodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitcode/GitcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
